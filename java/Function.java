7ae7952f4a6b7c4493bd974d3710c4665e295159
iconesalut <30355619+iconesalut@users.noreply.github.com>
2018-02-02 09:13:31 +0000

    create Function.java inspired by the python object Function in IOT.py

--- java/Function.java
+++ java/Function.java
@@ -1,1 +1,55 @@
 
+public class Function{
+	private Section m_section;
+	private int m_address;
+	private int m_size;
+	private String m_name;
+	private String m_label;
+	private Instruction[] m_instructions;
+	private Executable m_executable;
+	
+	public Function(Executable executable, int address, int size, String name){
+		m_executable = executable;
+		m_size = size;
+		m_address = address;
+		m_name = name;
+		m_label = "";
+		
+		m_section = m_execuable.findSection(m_address);
+	}
+	public Executable executable(){
+		return m_executable;
+	}
+	public int address(){
+		return m_address;
+	}
+	public int size(){
+		return m_size;
+	}
+	public Section section(){
+		return m_section;
+	}
+	public void addInstruction(Instruction instruction){
+	}
+	public native Instruction(int);
+	public String toString(){
+		String result = "";
+		if(m_section.name == ".text"){
+			result = "\t.text";
+		}
+		else{
+			result = "\t.section "+m_secton.name;
+		}
+		result += "\n\t.globl "+m_name+"\n\t.type"+m_name+", @function\n"+m_name+":\ n";
+		/*if(m_label != ""){
+			result += m_label;
+		}*/
+		
+		for(Instruction instruction : m_instructions){
+			result += instruction.toString();
+		}
+		
+		result += "\t.size "+m_name+", .-"+m_name+"\n";
+		return result;
+	}
+}
